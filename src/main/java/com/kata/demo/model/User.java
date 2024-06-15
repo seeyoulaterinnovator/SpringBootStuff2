09dfc51265c1a748f173680b1984f1b40fca6c64
trpl <fsocietyyy@mail.ru>
2024-06-15 03:45:15 +0300

    Salamanca 2

--- src/main/java/com/kata/demo/model/User.java
+++ src/main/java/com/kata/demo/model/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 @Table(name="users")
 public class User{
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "firstname")
